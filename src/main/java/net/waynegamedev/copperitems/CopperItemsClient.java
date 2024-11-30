90923c58d79b6adc4679423fc087f4f5be0b84b1
WayneGameDev <wuwei@dipipa.com>
2024-11-30 14:26:11 +0800

    Initial commit

---
+++ src/main/java/net/waynegamedev/copperitems/CopperItemsClient.java
@@ -0,0 +1,10 @@
+package net.waynegamedev.copperitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperItemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
